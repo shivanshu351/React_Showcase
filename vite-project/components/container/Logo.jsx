a0190d54e02886a9e27a0a57cd91bdd6134bc9ef
Shivanshu Singh <shivanshusingh71@gmail.com>
2024-10-27 18:18:45 +0530

    "changed aagin"

--- vite-project/components/container/Logo.jsx
+++ vite-project/components/container/Logo.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Logo({ width = '100px' }) {
   return (
     <div>
-      <img src='https://unsplash.com/photos/a-bunch-of-balloons-that-are-shaped-like-email-7NT4EDSI5Ok'  alt="Logo" style={{ width }} />
+      <img src='https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  alt="Logo" style={{ width }} />
     </div>
   );
 }
