1998fa365e1af619b86680f35aa1be1338730279
Shivanshu Singh <shivanshusingh71@gmail.com>
2024-10-27 18:16:12 +0530

    "image changed"

--- vite-project/components/container/Logo.jsx
+++ vite-project/components/container/Logo.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Logo({ width = '100px' }) {
   return (
     <div>
-      <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.creativefabrica.com%2Fproduct%2Fcustom-lettering-logo-inspiration%2F&psig=AOvVaw0wmtFDuxpZkhYc1_3DOKmI&ust=1730119139542000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiGtJjKrokDFQAAAAAdAAAAABAE'  alt="Logo" style={{ width }} />
+      <img src='https://freeimage.host/i/2B3fhan'  alt="Logo" style={{ width }} />
     </div>
   );
 }
