801f3a1115a6713be701b7397d35df0f75e23495
Shivanshu Singh <shivanshusingh71@gmail.com>
2024-10-27 18:17:46 +0530

    "changed"

--- vite-project/components/container/Logo.jsx
+++ vite-project/components/container/Logo.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Logo({ width = '100px' }) {
   return (
     <div>
-      <img src='https://freeimage.host/i/2B3fhan'  alt="Logo" style={{ width }} />
+      <img src='https://unsplash.com/photos/a-bunch-of-balloons-that-are-shaped-like-email-7NT4EDSI5Ok'  alt="Logo" style={{ width }} />
     </div>
   );
 }
